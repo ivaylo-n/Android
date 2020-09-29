d46a5afaf5fbed858e7b16859fac3961bc2d271e
ivaylo-n <5083752+ivaylo-n@users.noreply.github.com>
2020-09-30 00:37:08 +0300

    Add Styles and Themes for Tip Calculator App

--- Android Basics in Kotlin by Google/TipTimeApp/app/src/main/java/com/example/tiptimeapp/MainActivity.kt
+++ Android Basics in Kotlin by Google/TipTimeApp/app/src/main/java/com/example/tiptimeapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptimeapp
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptimeapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -16,13 +20,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
+
     }
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
-            binding.tipResult.text = ""
+            displayTip(0.0)
             return
         }
 
@@ -37,7 +49,22 @@ class MainActivity : AppCompatActivity() {
             tip = kotlin.math.ceil(tip)
         }
 
+        displayTip(tip)
+
+    }
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int) : Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
